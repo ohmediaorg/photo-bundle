8f324b76564e9baf6c2527bd1de9191ff169744c
Justin Hoffman <justin@ohmedia.ca>
2024-05-02 11:35:27 -0600

    naming fix

--- src/Service/GalleryNavItemProvider.php
+++ src/Service/GalleryNavItemProvider.php
@@ -3,14 +3,14 @@
 namespace OHMedia\PhotoBundle\Service;
 
 use OHMedia\BackendBundle\Service\AbstractNavItemProvider;
+use OHMedia\BootstrapBundle\Component\Nav\NavItemInterface;
 use OHMedia\BootstrapBundle\Component\Nav\NavLink;
-use OHMedia\BootstrapBundle\Component\Nav\NavPhotoInterface;
 use OHMedia\PhotoBundle\Entity\Gallery;
 use OHMedia\PhotoBundle\Security\Voter\GalleryVoter;
 
 class GalleryNavItemProvider extends AbstractNavItemProvider
 {
-    public function getNavPhoto(): ?NavPhotoInterface
+    public function getNavItem(): ?NavItemInterface
     {
         if ($this->isGranted(GalleryVoter::INDEX, new Gallery())) {
             return (new NavLink('Photos', 'gallery_index'))
